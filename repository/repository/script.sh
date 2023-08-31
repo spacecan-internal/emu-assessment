f66a4fc4363e6f16a1a60a6cef93a69e4204d6c9
Jasper Nygaard <jasper@sovs.net>
2023-08-31 11:45:02 +0200

    Added new workflow for org, csv and file options (#88)

---
+++ repository/repository/script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# create ../../reports if it doesn't exist
+mkdir -p ../../reports
+
+DEST=../../reports/repos.json
+echo "[]" >$DEST
+REPOS=$(echo "$2" | tr ',' '\n')
+
+# iterate over REPOS
+for REPO in $REPOS; do
+  echo "Getting repo: $REPO"
+  REPOSITORIES=$(gh api graphql \
+    -H X-Github-Next-Global-ID:true \
+    -F owner="${1}" \
+    -F name="$REPO" \
+    -f query='query getRepo($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+      name
+      nameWithOwner
+      visibility
+      isFork
+      hasProjectsEnabled
+      hasDiscussionsEnabled
+      diskUsage
+      updatedAt
+      gitattributes: object(expression: "HEAD:.gitattributes") {
+        __typename
+      }
+      lfsconfig: object(expression: "HEAD:.lfsconfig") {
+        __typename
+      }
+    }
+  }' \
+    --jq '.data.repository' |
+    jq -s '.')
+  echo "$REPOSITORIES" >repo.json
+  cp $DEST tmp.json
+  jq -sc add tmp.json repo.json >$DEST
+  rm -rf repo.json tmp.json
+done
