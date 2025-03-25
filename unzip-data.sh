4a452ceb5f2b9dfabe0d0ec4a14a13650e4a53b0
Ahmed Magdy <ahmed.magdy@solidify.dev>
2025-03-25 14:48:58 +0100

    Remove extra lines

--- unzip-data.sh
+++ unzip-data.sh
@@ -15,17 +15,5 @@ fi
 # Read all zip files in the source folder and unzip them to a destination folder
 find "$SOURCE_FOLDER" -type f -name "*.zip" -exec unzip -o {} -d "$DESTINATION_FOLDER" \;
 
-# # Get file names
-# ZIP_FILES=$(find "$SOURCE_FOLDER" -type f -name "*.zip" -exec basename {} \;);
-# for i in ${ZIP_FILES}; do
-# 	unzip -o "${SOURCE_FOLDER}/${i}" -d "${DESTINATION_FOLDER}"
-# done
-
 # Read all the json files in the destination folder and format them inplace using jq
 find "$DESTINATION_FOLDER" -type f -name "*.json" -exec sh -c 'jq . "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
-
-# JSON_FILES=$(find "$DESTINATION_FOLDER" -type f -name "*.json" -exec basename {} \;);
-# for i in ${JSON_FILES}; do
-# 	# shellcheck disable=SC2094
-# 	jq . "${DESTINATION_FOLDER}/${i}" > "${DESTINATION_FOLDER}/${i}"
-# done
